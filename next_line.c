84ad4b23b100d5574a5830e500e638b4eca1d51e
Merel Mourik <mmourik@f1r5s7.codam.nl>
2020-06-05 17:03:16 +0200

    stuck in a loop

--- next_line.c
+++ next_line.c
@@ -6,103 +6,70 @@
 /*   By: merelmourik <merelmourik@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/29 11:43:11 by merelmourik   #+#    #+#                 */
-/*   Updated: 2020/06/05 13:55:53 by mmourik       ########   odam.nl         */
+/*   Updated: 2020/06/05 17:02:45 by mmourik       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+#include "./libft/libft.h"
 
-static int		ft_strlen(char *str)
+static char	*ft_realloc(char *input, int size)
 {
-	int i;
+	char *tmp_str;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+	tmp_str = ft_strdup(input);
+	if (!tmp_str)
+		exit (1);
+	free(input);
+	input = malloc(sizeof(char) * size);
+	if (!input)
+		exit (1);
+	input = tmp_str;
+	return (input);
 }
 
-static char		*ft_dup(char *str)
-{
-	int		i;
-	char	*dup;
-
-	i = ft_strlen(str);
-	if (!(dup = malloc(sizeof(char) * (i + 1))))
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		dup[i] = str[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-
-static char		*ft_strjoin(char *s1, char *s2)
-{
-	int		i;
-	int		j;
-	char	*new;
-
-	i = ft_strlen(s1);
-	j = ft_strlen(s2);
-	new = malloc(sizeof(char) * (i + j + 1));
-	if (!new)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		new[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		new[i + j] = s2[j];
-		j++;
-	}
-	new[i + j] = '\0';
-	free(s1);
-	return (new);
-}
-
-static int		ft_read(int fd, char **input)
+static int	ft_read(int fd, char **input)
 {
 	char	*buffer;
 	int		ret;
+	int		pos;
 
-	buffer = malloc(sizeof(char) * 1024);			//kan dus maar een x aantal characters lezen
-	if (buffer == NULL)
+	pos = 0;
+	while (1)
 	{
-		free(input);
-		return (-2);
-	}
-	ret = read(fd, buffer, 1023);
-	if (ret == -1)
-	{
-		free(*input);
+		buffer = malloc(sizeof(char) * 1024);			//kan dus maar een x aantal characters lezen
+		if (buffer == NULL)
+		{
+			free(input);
+			return (-2);
+		}
+		ret = read(fd, buffer, 1023);
+		if (ret == 0 || buffer[pos] == '\n')
+			return (ret);
+		if (ret == -1)
+		{
+			free(*input);
+			free(buffer);
+			return (-2);
+		}
+		ret--;
+		buffer[ret] = '\0';
+		*input = ft_strjoin(*input, buffer);
 		free(buffer);
-		return (-2);
+		if (input == NULL)
+			return (-2);
+		pos++;
+		if (pos >= 1024)
+			buffer = ft_realloc(buffer, 1024);
 	}
-	ret--;
-	buffer[ret] = '\0';
-	*input = ft_strjoin(*input, buffer);
-	if (input == NULL)
-	{
-		free(buffer);
-		return (-2);
-	}
-	free(buffer);
-	return (ret);
+	return (0);
 }
 
-int				next_line(int fd, char **input)
+int			next_line(int fd, char **input)
 {
 	int ret;
 
-	*input = ft_dup("");
+	*input = ft_strdup("");
 	if (input == NULL)
 		return (-2);
 	ret = ft_read(fd, input);
