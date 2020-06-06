d31fe2969392a570f6d1c55e6e523f032a73fc8b
Merel Mourik <mmourik@f0r6s6.codam.nl>
2020-06-06 12:14:40 +0200

    next with realloc

--- next_line.c
+++ next_line.c
@@ -6,12 +6,14 @@
 /*   By: merelmourik <merelmourik@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/29 11:43:11 by merelmourik   #+#    #+#                 */
-/*   Updated: 2020/06/05 17:02:45 by mmourik       ########   odam.nl         */
+/*   Updated: 2020/06/06 12:13:27 by mmourik       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "./libft/libft.h"
+
+#define BUFF_SIZE 1024
 
 static char	*ft_realloc(char *input, int size)
 {
@@ -19,50 +21,52 @@ static char	*ft_realloc(char *input, int size)
 
 	tmp_str = ft_strdup(input);
 	if (!tmp_str)
-		exit (1);
+		exit(1);
 	free(input);
 	input = malloc(sizeof(char) * size);
 	if (!input)
-		exit (1);
+		exit(1);
 	input = tmp_str;
 	return (input);
 }
 
-static int	ft_read(int fd, char **input)
+static int		ft_read(int fd, char **input)
 {
 	char	*buffer;
 	int		ret;
-	int		pos;
+	int		buff_size;
 
-	pos = 0;
-	while (1)
+	buff_size = BUFF_SIZE;
+	buffer = malloc(sizeof(char) * BUFF_SIZE);
+	if (buffer == NULL)
 	{
-		buffer = malloc(sizeof(char) * 1024);			//kan dus maar een x aantal characters lezen
-		if (buffer == NULL)
-		{
-			free(input);
-			return (-2);
-		}
-		ret = read(fd, buffer, 1023);
-		if (ret == 0 || buffer[pos] == '\n')
-			return (ret);
-		if (ret == -1)
-		{
-			free(*input);
-			free(buffer);
-			return (-2);
-		}
-		ret--;
-		buffer[ret] = '\0';
-		*input = ft_strjoin(*input, buffer);
-		free(buffer);
-		if (input == NULL)
-			return (-2);
-		pos++;
-		if (pos >= 1024)
-			buffer = ft_realloc(buffer, 1024);
+		free(input);
+		return (-2);
 	}
-	return (0);
+	ret = read(fd, buffer, (buff_size - 1));
+	if (ret >= BUFF_SIZE)
+		while (ret >= buff_size)
+		{
+			buff_size += BUFF_SIZE;
+			buffer = ft_realloc(buffer, buff_size);
+			ret = read(fd, buffer, (buff_size - 1));
+		}
+	if (ret == -1)
+	{
+		free(*input);
+		free(buffer);
+		return (-2);
+	}
+	ret--;
+	buffer[ret] = '\0';
+	*input = ft_strjoin(*input, buffer);
+	if (input == NULL)
+	{
+		free(buffer);
+		return (-2);
+	}
+	free(buffer);
+	return (ret);
 }
 
 int			next_line(int fd, char **input)
@@ -72,6 +76,6 @@ int			next_line(int fd, char **input)
 	*input = ft_strdup("");
 	if (input == NULL)
 		return (-2);
-	ret = ft_read(fd, input);
+	ret = ft_read(fd, input);			
 	return ((ret == -2) ? -1 : 0);
 }
