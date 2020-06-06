b4533438a9f51058b56965c8477ebe9c5e2a1f77
Sam Kortekaas <samkortekaas@protonmail.com>
2020-06-06 12:05:36 +0200

    Parser is now expexting that you skip to the next node from the command, and NOT in the parse function. Except for when the symbol is ;

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/04 14:33:37 by samkortekaa   #+#    #+#                 */
-/*   Updated: 2020/06/05 17:34:01 by skorteka      ########   odam.nl         */
+/*   Updated: 2020/06/06 12:03:36 by skorteka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,40 @@ t_node *execute_cmd(t_node *ptr)
 	if (ptr->command == ECHO)
 		ptr = echo(ptr);
 	else if (ptr->command == CD)
+	{
+		ptr = ptr->next;
 		write(1, "Executed cd\n", 12);
+	}
 	else if (ptr->command == PWD)
+	{
+		ptr = ptr->next;
 		write(1, "Executed pwd\n", 13);
+	}
 	else if (ptr->command == EXPORT)
+	{
+		ptr = ptr->next;
 		write(1, "Executed export\n", 16);
+	}
 	else if (ptr->command == UNSET)
+	{
+		ptr = ptr->next;
 		write(1, "Executed unset\n", 15);
+	}
 	else if (ptr->command == ENV)
+	{
+		ptr = ptr->next;
 		write(1, "Executed env\n", 13);
+	}
 	else if (ptr->command == EXIT)
+	{
+		ptr = ptr->next;
 		write(1, "Executed exit\n", 14);
+	}
 	else
+	{
+		ptr = ptr->next;
 		write(1, "Command not recognized\n", 23);
+	}
 	return (ptr);
 }
 
@@ -43,13 +64,8 @@ void	parse(t_node *cmd_list)
 	ptr = cmd_list;
 	while (ptr)
 	{
-		if (ptr->type == COMMAND)
-		{
-			ptr = execute_cmd(ptr);
-		}
-		else			//is deze nodig? misschien error van maken
-			write(1, "Command not recognized\n", 23);
-		if (ptr && ptr->command != SEMICOLON)
+		ptr = execute_cmd(ptr);
+		if (ptr && ptr->command == SEMICOLON)
 			ptr = ptr->next;
 	}
 }
