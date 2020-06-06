358e147f24203601f3e35a99e477237fe91437bb
Sam Kortekaas <samkortekaas@protonmail.com>
2020-06-06 14:26:17 +0200

    WIP

--- cd.c
+++ cd.c
@@ -6,7 +6,7 @@
 /*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/06 13:52:18 by skorteka      #+#    #+#                 */
-/*   Updated: 2020/06/06 14:18:41 by skorteka      ########   odam.nl         */
+/*   Updated: 2020/06/06 14:22:00 by skorteka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "./libft/libft.h"
 #include "errno.h"
 #include <unistd.h>
+#include <string.h>
 
 
 t_node *cd(t_node *node)
@@ -27,8 +28,10 @@ t_node *cd(t_node *node)
 	path = node->data;
 	if (chdir(path))
 	{
-		ft_printf("Error: %s", strerror(errno));
+		ft_printf("Error: %s\n", strerror(errno));
 		errno = 0;
 	}
+	while (node && node->command != SEMICOLON)
+		node = node->next;
 	return (node);
 }
--- cd.h
+++ cd.h
@@ -6,13 +6,14 @@
 /*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/06 13:52:18 by skorteka      #+#    #+#                 */
-/*   Updated: 2020/06/06 13:55:31 by skorteka      ########   odam.nl         */
+/*   Updated: 2020/06/06 14:19:35 by skorteka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CD_H
 # define CD_H
-
 #include "lexer.h"
+
+t_node *cd(t_node *node);
 
 #endif
