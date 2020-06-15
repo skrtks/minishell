c2cc7f3c195824f4ccd1f29e78540d01d214e8fd
merel mourik <merelmourik@mbp-van-merel.home>
2020-06-15 15:39:34 +0200

    to_list geupdate

--- export.c
+++ export.c
@@ -6,7 +6,7 @@
 /*   By: merelmourik <merelmourik@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/06 15:41:37 by mmourik       #+#    #+#                 */
-/*   Updated: 2020/06/15 10:56:16 by merelmourik   ########   odam.nl         */
+/*   Updated: 2020/06/15 15:21:42 by merelmourik   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
