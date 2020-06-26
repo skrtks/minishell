1b10abe0a940248643eb37c3de81ec769320e53e
merel mourik <merelmourik@mbp-van-merel.home>
2020-06-26 16:08:59 +0200

    changed the includes

--- redirection.c
+++ redirection.c
@@ -6,15 +6,12 @@
 /*   By: merelmourik <merelmourik@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/19 13:15:53 by merelmourik   #+#    #+#                 */
-/*   Updated: 2020/06/22 11:09:00 by merelmourik   ########   odam.nl         */
+/*   Updated: 2020/06/26 16:08:17 by merelmourik   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lexer.h"
-
 #include <fcntl.h>
 #include "libft/libft.h"
-#include <stdio.h>
 #include <string.h>
 #include "parser.h"
 
