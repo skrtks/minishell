fc3b3ecf0ab06c50fe052667a9d798b9375c756d
Sam Kortekaas <samkortekaas@protonmail.com>
2020-07-01 14:08:10 +0200

    Removed double function

--- utils/other_utils.c
+++ utils/other_utils.c
@@ -6,7 +6,7 @@
 /*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/17 11:29:29 by merelmourik   #+#    #+#                 */
-/*   Updated: 2020/07/01 14:04:07 by skorteka      ########   odam.nl         */
+/*   Updated: 2020/07/01 14:07:27 by skorteka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,6 @@
 t_node	*free_on_error(char *cmd)
 {
 	free(cmd);
-	return (NULL);
-}
-
-char	*check_spec_char(const char *str, int c)
-{
-	const char		*src;
-	size_t			count;
-
-	src = str;
-	count = 0;
-	while (src[count] != '\0')
-	{
-		if (src[count] == (char)c)
-			return ((char *)str);
-		count++;
-		str++;
-	}
 	return (NULL);
 }
 
