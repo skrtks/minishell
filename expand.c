93ee7c6fcc6fd931489243dfad267330cd74a710
Sam Kortekaas <samkortekaas@protonmail.com>
2020-07-02 15:52:54 +0200

    Shortened function

--- expand.c
+++ expand.c
@@ -6,34 +6,12 @@
 /*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/27 16:08:36 by sam           #+#    #+#                 */
-/*   Updated: 2020/07/02 15:02:07 by skorteka      ########   odam.nl         */
+/*   Updated: 2020/07/02 15:51:45 by skorteka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "lexer.h"
-
-// TODO: Bij input met ${ x} worden twee nodes aangemaakt met ${ en x}. Dat moet even anders.
-
-// int check_braces(char *str)
-// {
-//     int i;
-
-//     i = 0;
-// 	while (str[i] != '\0')
-// 	{
-// 		if (str[i] == '}' && str[i - 1] != '\\')
-// 			return (i);
-// 		if (ft_strchr("@^*+[]:,./?~ ", str[i]))
-// 		{
-// 			ft_printf("minishell: ${%s: bad substitution\n", str);
-// 			return (-1);
-// 		}
-// 		i++;
-// 	}
-// 	ft_printf("Braces not closed\n");
-//     return (-1);
-// }
 
 int get_len(char *str)
 {
@@ -48,6 +26,18 @@ int get_len(char *str)
 	}
 	return (i);
 }
+
+int clean(char *str, char *str1, char *str2, int ret_code)
+{
+	if (str)
+		free(str);
+	if (str1)
+		free(str1);
+	if (str2)
+		free(str2);
+	return (ret_code);
+}
+
 
 int do_expansion(t_node *cmd_ptr, char *id_str, t_env *env_list, int id_len)
 {
@@ -64,21 +54,12 @@ int do_expansion(t_node *cmd_ptr, char *id_str, t_env *env_list, int id_len)
 		{
 			if (!(repl_str = ft_substr(env_list->data, id_len + 1,
 								ft_strlen(env_list->data + id_len + 1))))
-			{
-				free(id);
-				return (1);
-			}
+				return (clean(id, NULL, NULL, 1));
 			tmp = ft_strdup(cmd_ptr->data);
 			free(cmd_ptr->data);
 			if (!(cmd_ptr->data = ft_strjoin(repl_str, tmp)))
-			{
-				free(id);
-				free(repl_str);
-				free(tmp);
-				return (1);
-			}
-			free(repl_str);
-			free(tmp);
+				return (clean(id, repl_str, tmp, 1));
+			clean(repl_str, tmp, NULL, 0);
 			break ;
 		}
 		env_list = env_list->next;
@@ -87,29 +68,31 @@ int do_expansion(t_node *cmd_ptr, char *id_str, t_env *env_list, int id_len)
 	return (0);
 }
 
-void do_tilde_expansion(t_node *cmd_ptr, t_env *env_list)
+int do_tilde_expansion(t_node *cmd_ptr, t_env *env_list)
 {
 	char *path;
 	char *repl_str;
 
 	if (cmd_ptr->data[1] != '\0' && ft_strchr("@^*+[]{},.?", cmd_ptr->data[1]))
-		return ;
+		return (0);
 	path = NULL;
 	while (env_list)
 	{
 		if (!ft_strncmp("HOME=", env_list->data, 5))
 		{
-			path = ft_strdup(env_list->data + 5);
+			if (!(path = ft_strdup(env_list->data + 5)))
+				return (1);
 			break ;
 		}
 		env_list = env_list->next;
 	}
-	repl_str = ft_strjoin(path, cmd_ptr->data + 1);
+	if (!(repl_str = ft_strjoin(path, cmd_ptr->data + 1)))
+		return (clean(path, NULL, NULL, 1));
 	free(cmd_ptr->data);
 	cmd_ptr->data = repl_str;
 	if (path)
 		free(path);
-	// return (paths_arr);
+	return (0);
 }
 
 int expand(t_node *node, t_env *env_list)
@@ -128,7 +111,8 @@ int expand(t_node *node, t_env *env_list)
 			return (0);
 		else if (cmd_ptr->data[0] == '~')
 		{
-			do_tilde_expansion(cmd_ptr, env_list);
+			if (do_tilde_expansion(cmd_ptr, env_list))
+				return (1);
 		}
 		else if (cmd_ptr->data[0] == '$')
 		{
@@ -138,10 +122,7 @@ int expand(t_node *node, t_env *env_list)
 			free (cmd_ptr->data);
 			cmd_ptr->data = ft_strdup(id_str + id_len);
 			if (do_expansion(cmd_ptr, id_str, env_list, id_len))
-			{
-				free(id_str);
-				return (1);
-			}
+				return (clean(id_str, NULL, NULL, 1));
 			free (id_str);
 		}
 		cmd_ptr = cmd_ptr->next;
