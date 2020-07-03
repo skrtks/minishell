bbbf156ca0570b79bbe60730e1b39b0516ebd204
Sam Kortekaas <samkortekaas@protonmail.com>
2020-07-03 15:53:03 +0200

    Exit now behaving like bash

--- builtins/exit.c
+++ builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   exit.c                                             :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: sam <sam@student.codam.nl>                   +#+                     */
+/*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/09 15:21:39 by sam           #+#    #+#                 */
-/*   Updated: 2020/06/26 21:35:34 by sam           ########   odam.nl         */
+/*   Updated: 2020/07/03 15:23:39 by skorteka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,24 @@
 
 void	exit_shell(t_node *cmd_lst, t_env **env_lst, t_env **exp_lst, int code)
 {
+	int i;
+
+    ft_printf("exit\n");
+    if (cmd_lst && cmd_lst->next)
+    {
+        i = 0;
+        while(cmd_lst->next->data[i])
+        {
+            if (!ft_isdigit(cmd_lst->next->data[i]))
+            {
+                ft_printf("minishell: exit: %s: numeric argument required\n",\
+					cmd_lst->next->data);
+                break ;
+            }
+            i++;
+        }
+        code = ft_atoi(cmd_lst->next->data);
+    }
 	if (env_lst)
 		free_envlist(env_lst);
 	if (exp_lst)
