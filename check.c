e9fea7407504c2027ac0ea6afd47783e153c2d8e
Sam Kortekaas <samkortekaas@protonmail.com>
2020-07-08 14:44:39 +0200

    Some norme cleanup

--- check.c
+++ check.c
@@ -14,16 +14,46 @@
 #include "lexer.h"
 #include "utils/utils.h"
 
-int check_redir(const t_node *ptr) {
+int	check_redir(const t_node *ptr)
+{
 	if (ptr->type == REDIR && ptr->next == NULL)
 	{
-		ft_printf("minishell: syntax error near unexpected token `newline'\n");
+		ft_printf("minishell: syntax error near unexpected ");
+		ft_printf("token `newline'\n");
 			return (1);
 	}
 	return (0);
 }
 
-int check_cmd_list(t_node *cmd_list)
+int	check_current(const t_node *ptr, int *prev_is_symbol, int *prev_is_red,
+				const int *new_cmd)
+{
+	if ((ptr->type == SYMBOL && (*prev_is_symbol || *prev_is_red)) ||
+	(ptr->type == REDIR && (*prev_is_red) && !*new_cmd))
+	{
+		ft_printf("minishell: syntax error near unexpected token `%s'\n",
+				ptr->data);
+			return (1); // Exit code? in bash 2 bij syntax error
+	}
+	else if (ptr->type == SYMBOL)
+	{
+		*prev_is_red = 0;
+		*prev_is_symbol = 1;
+	}
+	else if (ptr->type == REDIR)
+	{
+		*prev_is_symbol = 0;
+		*prev_is_red = 1;
+	}
+	else
+	{
+		*prev_is_symbol = 0;
+		*prev_is_red = 0;
+	}
+	return (0);
+}
+
+int	check_cmd_list(t_node *cmd_list)
 {
 	t_node	*ptr;
 	int		prev_is_symbol;
@@ -38,27 +68,8 @@ int check_cmd_list(t_node *cmd_list)
 	{
 		if (check_redir(ptr))
 			return (1); // Exit code? in bash 2 bij syntax error
-		if ((ptr->type == SYMBOL && (prev_is_symbol || prev_is_red)) ||
-			(ptr->type == REDIR && (prev_is_red) && !new_cmd))
-		{
-			ft_printf("minishell: syntax error near unexpected token `%s'\n", ptr->data);
+		if (check_current(ptr, &prev_is_symbol, &prev_is_red, &new_cmd))
 			return (1); // Exit code? in bash 2 bij syntax error
-		}
-		else if (ptr->type == SYMBOL)
-		{
-			prev_is_red = 0;
-			prev_is_symbol = 1;
-		}
-		else if (ptr->type == REDIR)
-		{
-			prev_is_symbol = 0;
-			prev_is_red = 1;
-		}
-		else
-		{
-			prev_is_symbol = 0;
-			prev_is_red = 0;
-		}
 		ptr = ptr->next;
 		if (ptr && ptr->command == SEMICOLON)
 			new_cmd = 1;
